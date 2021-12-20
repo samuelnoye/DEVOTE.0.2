6cdd844eb7b4e38009216d0d53532cec7f0f00c6
Samuel Noye <samuel.noye@amalitech.org>
2021-12-20 15:02:29 +0000

    Hide

---
+++ DEVOTE.0.2/Extentions/HideKeyboard.swift
@@ -0,0 +1,17 @@
+//
+//  HideKeyboard.swift
+//  DEVOTE.0.2
+//
+//  Created by Samuel Noye on 20/12/2021.
+//
+
+
+import SwiftUI
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
